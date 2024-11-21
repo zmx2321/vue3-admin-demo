3efa1af3529cbea3ebcd9d34b384ee8dc12977ea
zmx2321 <zmx2321@163.com>
2024-11-21 16:06:00 +0800

    fix: init

---
+++ src/views/business/serviceArea/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card content-box">
+    <span class="text">服务区平面图 🍓🍇🍈🍉</span>
+    <el-input v-model="value" placeholder="测试缓存"></el-input>
+  </div>
+</template>
+
+<script setup lang="ts" name="serviceArea">
+import { ref } from "vue";
+const value = ref<string>("");
+</script>
+
+<style scoped lang="scss">
+@import "./index";
+</style>
