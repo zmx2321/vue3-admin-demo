12d9daad427d9dc9171bc1ed69aee9e7c948cd34
zmx2321 <zmx2321@163.com>
2024-11-29 13:45:08 +0800

    deploy

---
+++ assets/js/useAuthButtons-B5JQa7td.js
@@ -0,0 +1,1 @@
+import{C as r,aj as s,x as n}from"./index-Dn_xEMn3.js";const B=()=>{const u=r(),o=s().authButtonListGet[u.name]||[];return{BUTTONS:n(()=>{let t={};return o.forEach(e=>t[e]=!0),t})}};export{B as u};
